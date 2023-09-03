4d2446c63e3aad60981a1ad0ba2156c88d78ba66
guardiangel <felix.sgq@gmail.com>
2023-09-03 00:04:59 -0400

    Add esapi for logging because of security reason

--- src/main/java/com/felix/springbootdemo/SpringBootDemoRunLister.java
+++ src/main/java/com/felix/springbootdemo/SpringBootDemoRunLister.java
@@ -1,5 +1,7 @@
 package com.felix.springbootdemo;
 
+import org.owasp.esapi.ESAPI;
+import org.owasp.esapi.Logger;
 import org.springframework.boot.ConfigurableBootstrapContext;
 import org.springframework.boot.SpringApplicationRunListener;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -7,18 +9,20 @@ import org.springframework.context.ConfigurableApplicationContext;
 import java.time.Duration;
 
 public class SpringBootDemoRunLister implements SpringApplicationRunListener {
+    private final Logger log = ESAPI.getLogger("SpringBootDemoRunLister");
+
     @Override
     public void starting(ConfigurableBootstrapContext bootstrapContext) {
-        System.err.println(">>>>>>>>>The service is starting......>>>>>>>>>>>");
+        log.info(Logger.EVENT_UNSPECIFIED,">>>>>>>>>The service is starting......>>>>>>>>>>>");
     }
 
     @Override
     public void started(ConfigurableApplicationContext context, Duration timeTaken) {
-        System.err.println(">>>>>>>>>The service started and prepare to provide service......>>>>>>>>>>>");
+        log.info(Logger.EVENT_SUCCESS,">>>>>>>>>The service started and prepare to provide service......>>>>>>>>>>>");
     }
 
     @Override
     public void failed(ConfigurableApplicationContext context, Throwable exception) {
-        System.err.println(">>>>>>>>>The service failed to start, the reason is:"+exception.getLocalizedMessage()+"......>>>>>>>>>>>");
+        log.error(Logger.EVENT_FAILURE,">>>>>>>>>The service failed to start, the reason is:"+exception.getLocalizedMessage()+"......>>>>>>>>>>>");
     }
 }
