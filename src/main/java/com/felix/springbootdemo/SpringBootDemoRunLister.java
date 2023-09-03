0e3a7cb0ad777848d89b33f8a4da152932606afa
guardiangel <felix.sgq@gmail.com>
2023-09-02 23:28:42 -0400

    Add service listener

--- src/main/java/com/felix/springbootdemo/SpringBootDemoRunLister.java
+++ src/main/java/com/felix/springbootdemo/SpringBootDemoRunLister.java
@@ -1,23 +1,12 @@
 package com.felix.springbootdemo;
 
 import org.springframework.boot.ConfigurableBootstrapContext;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringApplicationRunListener;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import java.time.Duration;
 
 public class SpringBootDemoRunLister implements SpringApplicationRunListener {
-
-    private final SpringApplication springApplication;
-
-    private final String[] args;
-
-    public SpringBootDemoRunLister(SpringApplication springApplication, String[] args) {
-        this.springApplication = springApplication;
-        this.args = args;
-    }
-
     @Override
     public void starting(ConfigurableBootstrapContext bootstrapContext) {
         System.err.println(">>>>>>>>>The service is starting......>>>>>>>>>>>");
